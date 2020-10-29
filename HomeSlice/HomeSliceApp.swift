644ee1aa88d2c9084e18836c2570d34c7fc77e89
Evan Lucas <evanlucas@me.com>
2020-10-29 08:46:29 -0500

    Initial Commit

---
+++ HomeSlice/HomeSliceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeSliceApp.swift
+//  HomeSlice
+//
+//  Created by Evan Lucas on 10/29/20.
+//
+
+import SwiftUI
+
+@main
+struct HomeSliceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
